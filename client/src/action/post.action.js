56dcfa0fa9d8f260af26d595b02f3cf83c8d91da
gaouimounir <mounir.gaoui@outlook.com>
2023-10-11 16:21:19 +0200

    ajout de ADD POSTS

--- client/src/action/post.action.js
+++ client/src/action/post.action.js
@@ -1,11 +1,23 @@
 import axios from "axios";
 export const GET_POSTS = "GET_POSTS";
+export const ADD_POSTS = "ADD_POSTS";
 
 export const getPosts = () => {
   return (dispatch) => {
     return axios.get("http://localhost:3000/posts").then((res) => {
       dispatch({
         type: GET_POSTS,
+        payload: res.data,
+      });
+    });
+  };
+};
+
+export const addPost = (data) => {
+  return (dispatch) => {
+    return axios.get("http://localhost:3000/posts", data).then((res) => {
+      dispatch({
+        type: ADD_POSTS,
         payload: res.data,
       });
     });
--- client/src/reducer/post.reducer.js
+++ client/src/reducer/post.reducer.js
@@ -1,9 +1,11 @@
-import { GET_POSTS } from "../action/post.action";
+import { ADD_POSTS, GET_POSTS } from "../action/post.action";
 
 const initialState = {};
 export default function postReducer(state = initialState, action) {
   switch (action.type) {
     case GET_POSTS:
+      return action.payload;
+    case ADD_POSTS:
       return action.payload;
     default:
       return state;
